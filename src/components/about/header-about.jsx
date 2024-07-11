39c814a41a5b4ff16d8664127ec20465787974b5
Hulkobain <114093499+Hulkobain@users.noreply.github.com>
2024-07-11 21:22:17 +0200

    Update header-about.jsx

--- src/components/about/header-about.jsx
+++ src/components/about/header-about.jsx
@@ -9,7 +9,7 @@ export function HeaderAbout() {
       <p className="mt-3 leading-relaxed text-gray-300 ">
         Hi! I'm Raul Romero, a passionate <span className="span-badge">FULL-STACK DEVELOPER</span>{" "}
         with experience in both front-end and back-end technologies. 
-        I focus on building robust and functional web applications using tools like JavaScript, React, Node.js and TypeScript.
+        I focus on building robust and functional web applications using tools like Springboot, Java, React, Angular, NodeJS and Typescript.
       </p>
     </div>
   )
