e4986fd91363227fc28cebcf7f99b61890790b24
Hulkobain <hulkito6@gmail.com>
2026-02-02 13:44:51 +0100

    Update Gratitude.jsx

--- src/components/gratitude/Gratitude.jsx
+++ src/components/gratitude/Gratitude.jsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Gratitude() {
   const emailHref = "mailto:raul.romero.6696@gmail.com";
-  const cvHref = "https://raulromero.netlify.app/images/CV_RaulRomero.pdf";
+  const cvHref = "/cv";
 
   return (
     <section className="my-8">
@@ -37,8 +37,6 @@ export function Gratitude() {
             <Link
               className="group relative inline-flex w-full items-center justify-center overflow-hidden rounded-xl bg-white px-6 py-2 font-bold text-black md:flex-1"
               href={cvHref}
-              rel="noreferrer"
-              target="_blank"
             >
               <span>CV</span>
               <div className="w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100"></div>
